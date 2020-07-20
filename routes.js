c6cc55cc4f20202ec32f7aa2df9f79229e1b3db4
Glitch (hello-express) <none>
2020-07-20 21:45:34 +0000

    ✋🌸 Checkpoint ./routes.js:17806960/334 ./views/layouts/main.hbs:17806960/1 ./views/partials/welcomemenu.hbs:17806960/853 ./views/index.hbs:17806960/545

--- routes.js
+++ routes.js
@@ -2,3 +2,14 @@
 
 const express = require("express");
 const router = express.Router();
+
+const accounts = require("./controllers/accounts.js");
+
+router.get("/", accounts.index);
+router.get("/login", accounts.login);
+router.get("/signup", accounts.signup);
+router.get("/logout", accounts.logout);
+router.post("/register", accounts.register);
+router.post("/authenticate", accounts.authenticate);
+
+module.exports = router;
