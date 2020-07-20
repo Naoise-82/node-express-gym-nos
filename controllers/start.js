b9a77a2bc10ce482e0f7601f40df5af3d51f1a82
Glitch (hello-express) <none>
2020-07-20 21:34:10 +0000

    🦂⏳ Checkpoint ./models/user-store.js:17806960/580 ./models/user-store.json:17806960/570 ./package.json:17806960/1467 ./controllers/start.js:17806960/348

---
+++ controllers/start.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const logger = require("../utils/logger");
+const uuid = require("uuid");
+const accounts = require("./accounts.js");
+
+const start = {
+  index(request, response) {
+    logger.info("start page rendering");
+    
+  }
+}
