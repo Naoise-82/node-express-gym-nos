77092ef2f3b38d90fa3216d90ded87235ab41aaf
Glitch (hello-express) <none>
2020-07-20 21:23:07 +0000

    ⛑🚦 Checkpoint ./views/layouts/main.hbs:17806960/524 ./views/partials/welcomemenu.hbs:17806960/317 ./views/index.hbs:17806960/1732 ./server.js:17806960/1796 ./routes.js:17806960/83 ./controllers/accounts.js:17806960/1383 ./models/json-store.js:17806960/966 ./utils/logger.js:17806960/340

--- server.js
+++ server.js
@@ -1,34 +1,29 @@
-// server.js
-// where your node app starts
+"use strict";
 
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
+const logger = require("./utils/logger");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
+
 const app = express();
-
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
-
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
+app.use(cookieParser());
+const exphbs = require("express-handlebars");
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("public"));
+app.use(fileUpload());
+app.engine(
+  ".hbs",
+  exphbs({
+    extname: ".hbs",
+    defaultLayout: "main"
+  })
+);
+app.set("view engine", ".hbs");
 
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
-});
+const routes = require("./routes");
+app.use("/", routes);
 
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
-});
-
-// listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
+const listener = app.listen(process.env.PORT || 4000, function() {
+  logger.info(`glitch-template-1 started on port ${listener.address().port}`);
 });
